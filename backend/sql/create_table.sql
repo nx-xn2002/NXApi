e3ce269f18eec19b9270a7769ce526b03b40203c
nx-xn2002 <1870230468@qq.com>
2025-01-12 17:31:11 +0800

    🗃️ Add api_info

--- backend/sql/create_table.sql
+++ backend/sql/create_table.sql
@@ -6,6 +6,23 @@ create database if not exists nx_api;
 -- 切换库
 use nx_api;
 
+-- 接口信息表
+create table if not exists api_info
+(
+    id              bigint auto_increment comment 'id' primary key,
+    name            varchar(256)                       not null comment '接口名称',
+    description     varchar(256)                       not null comment '接口描述',
+    url             varchar(512)                       null comment '接口地址',
+    request_header  text                               null comment '请求头',
+    response_header text                               null comment '响应头',
+    status          tinyint  default 0                 not null comment '接口状态 0-关闭 1-开启',
+    method          varchar(256)                       not null comment '请求类型',
+    user_id         bigint                             not null comment '创建人id',
+    create_time     datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    update_time     datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    is_deleted      tinyint  default 0                 not null comment '逻辑删除 默认 - 0'
+) comment '接口信息表';
+
 -- 用户表
 create table if not exists user
 (
--- backend/src/main/java/com/nx/nxapi/MainApplication.java
+++ backend/src/main/java/com/nx/nxapi/MainApplication.java
@@ -1,11 +1,17 @@
 package com.nx.nxapi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * 主类（项目启动入口）
@@ -17,10 +23,26 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @MapperScan("com.nx.nxapi.mapper")
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+@Slf4j
 public class MainApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(MainApplication.class);
+        ConfigurableApplicationContext application = app.run(args);
+        Environment env = application.getEnvironment();
+        log.info("""
+
+                        ----------------------------------------------------------
+                        \tApplication '{}' is running! Access URLs:
+                        \tDoc: \thttp://{}:{}{}/doc.html
+                        \tDoc: \thttp://localhost:{}{}/doc.html
+                        ----------------------------------------------------------""",
+                env.getProperty("spring.application.name"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                env.getProperty("server.servlet.context-path"),
+                env.getProperty("server.port"),
+                env.getProperty("server.servlet.context-path"));
     }
 
 }
